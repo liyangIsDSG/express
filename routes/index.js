79d33bb581d700257a732366acf76c2ad7f39942
Leon0309 <ly18482022657@gmail.com>
2021-03-09 00:08:27 +0800

    fix:'更改user.js'

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,47 @@
 var express = require('express');
 var router = express.Router();
-const Users = require('./users');
-
+const userModel = require('../lib/userModel')
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  Users.find({
-    name: '小明',
-  }, function (err, user) {
-    if (err) {
-      res.send('操作失败')
-      return
-    }
-    console.log(user)
-    res.send(user)
-  })
+router.get('/',  function (req, res, next) {
+  // appModel.find({
+  //   name: '小p',
+  // }, function (err, user) {
+  //   if (err) {
+  //     res.send('操作失败:')
+  //   }else{
+  //     console.log('查询成功')
+  //   }
+  
+  // })
 
-  Users.create([{
+  // userModel.create([{
+  //   name: "小l",
+  //   age: 20,
+  //   gender: "female",
+  //   address: "广东东莞"
+  // }], err => {
+  //   if (!err) {
+  //     console.log("文档插入成功")
+  //   }else{
+  //     console.log(err)
+  //   }
+  // })
+  // res.render('index', {
+  //   title: 'Express'
+  // });
+  userModel.create({
     name: "小l",
     age: 20,
     gender: "female",
     address: "广东东莞"
-  }], err => {
-    if (!err) {
-      console.log("文档插入成功")
+  },(err,user) => {
+    if(!err) {
+      console.log('插入成功')
+    }else{
+      console.log('插入失败')
     }
   })
-  // res.render('index', {
-  //   title: 'Express'
-  // });
-  // res.send('Hello World')
-});
+  res.send('Hello World')
+})
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,28 @@
-const mongoose = require('../lib/mongoose')
-// 连接mongodb
+var express = require('express');
+var users = express.Router();
+// const mongoose = require('mongoose')
+// // 连接mongodb
 // mongoose.createConnection("mongodb://106.53.25.94:27017/data", {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 //   useFindAndModify: false,
 //   useCreateIndex: true
 // });
-// // 实例化连接对象
+// // // 实例化连接对象
 // const db = mongoose.connection
 // db.on('error', console.error.bind(console, '连接错误：'))
 // db.once('open', (callback) => {
 //   console.log('MongoDB for user.js连接成功！！')
 // })
-// 创建schema
-const userSchema = new mongoose.Schema({
-  name: String,
-  pwd: String
+// // 创建schema
+// const userSchema = new mongoose.Schema({
+//   name: String,
+//   pwd: String
+// })
+// // // 创建model
+// const users = mongoose.model('users', userSchema) // users 为创建或选中的集合
+
+users.post('/login', function (req, res, next) {
 })
-// // 创建model
-const users = mongoose.model('users', userSchema) // users 为创建或选中的集合
 
 module.exports = users
