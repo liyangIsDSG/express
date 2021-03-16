63c8a19f6cceef4debb69acce303e626d27a38be
liyangIsDSG <820559736@qq.com>
2021-03-16 17:13:12 +0800

    feat:"article接口完成，nignx配置图片映射完成,ajax请求的cors报错解决中"

--- routes/article.js
+++ routes/article.js
@@ -9,7 +9,9 @@ let imgName = ''
 const storage = multer.diskStorage({
     destination(req, file, cb) {
         // 处理上传文件所存放的目录
-        cb(null, './public/images')
+        // cb(null, './public/images')
+        cb(null, '../images')
+
     },
     filename(req, file, cb) {
         // 处理上传文件所存文件的名称
@@ -17,7 +19,7 @@ const storage = multer.diskStorage({
         let ext = getExt(file.originalname)
         let realName = (new Date()).getTime() + '.' + ext
         cb(null, realName)
-        imgName = 'http://younglee666.com:8090/public/images/' + realName
+        imgName = 'http://younglee666.com:8091/public/images/' + realName
     }
 })
 
@@ -27,12 +29,26 @@ const upload = multer({
 
 // 获取文章
 router.get('/', (req, res, next) => {
+    // articleModel.create([{
+    //     title: 'giao',
+    //     context: 'giao',
+    //     imgSrc: 'asdasdasd'
+    // }])
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     articleModel.find({}, (err, article) => {
         if (!err) {
-
-            res.send(article)
+            let result = {
+                status: 200,
+                articles: article
+            }
+            res.json(result)
         }
     })
+
+
 })
 
 // 新增文章
@@ -44,19 +60,29 @@ router.post('/addArticles', (req, res) => {
     upload(req, res, function (err) {
         if (err instanceof multer.MulterError) {
             // A Multer error occurred when uploading.
+            res.send('A Multer error occurred when uploading.')
+            return
         } else if (err) {
             // An unknown error occurred when uploading.
+            res.send('An unknown error occurred when uploading.')
+            return
         }
         // console.log(req.body)
         console.log(req.file)
         console.log('imgName:', imgName)
+
         articleModel.create([{
             title: req.body.title,
             context: req.body.context,
-            imgSrc: imgName
+            imgSrc: 'asdasdasd'
         }], (err, resl) => {
+            console.log('插入数据执行之后的回调')
+            if (err) {
+                res.send('数据库插入失败')
+            }
             res.send('ok')
         })
+
         // Everything went fine.
     })
 
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const appModel = require('../lib/models/userModel');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+
+
   appModel.find({}, function (err, user) {
     if (err) {
       console.log('操作失败', err)
@@ -16,19 +18,18 @@ router.get('/', function (req, res, next) {
     console.log(user)
   })
 
-  // appModel.create([{
-  //   name: "小l",
-  //   age: 20,
-  //   gender: "female",
-  //   address: "广东东莞"
-  // }], err => {
-  //   if (!err) {
-  //     console.log("文档插入成功")
-  //   }
-  // })
-  // res.render('index', {
-  //   title: 'Express'
-  // });
+  appModel.create([{
+    name: "zhaorui",
+    pwd: "female",
+  }], err => {
+    if (!err) {
+      console.log("文档插入成功")
+    } else {
+      console.log('插入失败')
+    }
+  })
+
+
   // res.send('Hello World')
 });
 
