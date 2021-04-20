b64b5389e7f8416e5bea3787e24a0daee60e048a
Leon0309 <ly18482022657@gmail.com>
2021-04-20 14:22:14 +0800

    feat:修改接口完成

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By", ' 3.2.1')
+  res.header("X-Powered-By", 'Express')
   res.header("Content-Type", "application/json;charset=utf-8");
+  // res.header("Cache-Control","no-store")
+  res.header('Cache-Control', 'no-chache');
   next();
 });
 
--- routes/addVip.js
+++ routes/addVip.js
@@ -7,9 +7,9 @@ router.get('/getVipList', (req, res, next) => {
     console.log(req.body)
     let query = vipModel.find({});
     let page = Number(req.query.page);
-    // note: req拿到的 rows 不转Number,mongodb报错 
-    let rows = Number(req.query.rows);
-    let skips = (page - 1) * rows
+    // note: req拿到的 rows 不转 Number,mongodb报错 
+    let rows = Number(req.query.rows) || 10;
+    let skips = (page - 1) * rows || 1
     // 当前页显示的数据所有数据-前几页显示的数据
     query.skip(skips);
     query.limit(rows)
@@ -21,6 +21,8 @@ router.get('/getVipList', (req, res, next) => {
             // 计算数据总数
             vipModel.find(function (err, result) {
                 let jsonArray = {
+                    code: 200,
+                    msg: '操作成功',
                     vipList: rs,
                     total: result.length
                 };
@@ -32,36 +34,37 @@ router.get('/getVipList', (req, res, next) => {
 })
 // 新增会员
 router.post('/addVip', (req, res) => {
-        vipModel.create([{
-            name: req.body.name,
-            tel: req.body.tel,
-            integral: req.body.integral,
-            date: req.body.date,
-            isremove:false,
-        }], (err, resl) => {
-            console.log('插入数据执行之后的回调')
-            if (err) {
-                res.send('数据库插入失败')
-            }
-            res.send('ok')
+    vipModel.create([{
+        name: req.body.name,
+        tel: req.body.tel,
+        integral: req.body.integral,
+        date: req.body.date,
+        isremove: false,
+    }], (err, resl) => {
+        console.log('插入数据执行之后的回调')
+        if (err) {
+            res.send('数据库插入失败')
+        }
+        res.send({
+            code: 200,
+            msg: '操作成功'
         })
+    })
 
-        // Everything went fine.
+    // Everything went fine.
 
 })
 
 // 修改会员信息
-router.post('/modifyVipInfo',(req,res) => {
-    console.log(req.body._id)
-    vipModel.findOne({ "_id":req.body._id },'integral', function (err, vipInfo) {
-        console.log(vipInfo)
-        if (err) {
-            console.log('出错了')
-            return
-        };
-        console.log(vipInfo)
-      });
-      res.send('ok')
+router.post('/modifyVipInfo', async (req, res) => {
+    // doc: https://mongoosejs.com/docs/tutorials/findoneandupdate.html
+    let list = await vipModel.findOneAndUpdate({ "_id": req.body._id }, req.body, {
+        returnOriginal: false
+    });
+    res.json({
+        code: 200,
+        msg: '操作成功',
+    });
 
 })
 module.exports = router
